c1cfc82b20b5975c44221eda8255f82ebc6d0426
XRY <xyrjobs@outlook.com>
2016-09-20 09:39:02 +0800

    8.4.3 pass

---
+++ app/auth/forms.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+class LoginForm(Form):
+    email = StringField(u'邮箱', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField(u'密码', validators=[DataRequired()])
+    remember_me = BooleanField(u'保持登陆')
+    submit = SubmitField(u'登陆')
