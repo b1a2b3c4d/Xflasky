ce4ddc71ca6d5589690199e6ad2f5cbbcd624df4
XRY <xyrjobs@outlook.com>
2016-09-30 11:22:46 +0800

    8.6.2pass

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,9 +8,9 @@ from ..models import User
 
 class RegistrationForm(Form):
     email = StringField(u'邮箱', validators=[DataRequired(), Length(1,64), Email()])
-    username = StringField(u'用户名', validators=[DataRequired(), Length(1,64), Regexp('~[A-Za-z][A-Za-z0-9_.]*$',
+    username = StringField(u'用户名', validators=[DataRequired(), Length(1,64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$',
                                                                                         0,'Usernames must have only letters,'
-                                                                                          '\'numbers,dots or underscores')])
+                                                                                          'numbers,dots or underscores')])
     password = PasswordField(u'密码', validators=[DataRequired(), EqualTo('password2', message='Passwords must match')])
     password2 = PasswordField(u'确认密码', validators=[DataRequired()])
     submit = SubmitField(u'注册')
@@ -20,11 +20,12 @@ class RegistrationForm(Form):
             raise ValidationError(u'此邮件已被注册')
 
     def validate_username(self, field):
-        if User.query.filter_bu(username=field.data).first():
+        if User.query.filter_by(username=field.data).first():
             raise ValidationError(u'此用户名已被使用')
+
 
 class LoginForm(Form):
     email = StringField(u'邮箱', validators=[DataRequired(), Length(1, 64), Email()])
-    password = PasswordField(u'密码', validators=[DataRequired()])
+    password = PasswordField(u'密码y', validators=[DataRequired()])
     remember_me = BooleanField(u'保持登陆')
     submit = SubmitField(u'登陆')
--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,17 @@ from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from .forms import LoginForm, RegistrationForm
 from ..models import User, db
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
+from ..email import send_email
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -29,8 +39,43 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
         db.session.add(user)
-        flash(u'您现在可以登录了')
+        db.session.commit()
+        token = user.generate_comfirmation_token()
+        send_email(user.email, u'确认您的帐户',
+                   'auth/email/confirm',
+                   user=user, token=token)
+        flash(u'一封确认邮件已经发到你的邮箱')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
